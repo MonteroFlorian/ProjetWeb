3df6703ad36188ddf97a21cb1d76bdcfd5145648
Samuel Da Silva Leitao <dasilvas@3il.fr>
2022-10-17 16:49:50 +0200

    Mise en page avec cards

--- views/Visiteur/Metal.view.php
+++ views/Visiteur/Metal.view.php
@@ -1,25 +1,66 @@
 <h1>Accueil</h1>
 <div class="album py-5 bg-light">
-    <div class="container">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div class="container-fluid">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-12 g-12">
 
-            <div class="col-sm-4 mb-4">
+            <div class="col-sm-3 "style = "margin:0%; padding:0%;">
                 <div class="card">
                     <img class="background-image"  style = "background-image:url(public/Assets/images/Vignes.jpg); min-height:1000px;background-attachment:fixed;
   background-position:center; background-repeat:no-repeat; background-size:cover; z-index:5;">
                 </div>
             </div>
-
-            <div class="col-sm-4 mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-title">Il faut mettre quelque chose dedans</p>
-                    </div>
-                </div>
+        <div class="col-sm-6 ">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-12 g-12">
+            <div class="col-sm-6" >
+                 <div class="card" style="margin: 5px;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                           <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                       <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+            </div>
+            <div class="card" style="margin: 5px;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                           <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                       <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+            </div>
             </div>
 
-            <div class="col-sm-4 mb-4">
-            <div class="card">
+            <div class="col-sm-6">
+            <div class="card" style="wmargin: 5px;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                           <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                       <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+            </div>
+            <div class="card" style="margin: 5px;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                           <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                       <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+            </div>
+            </div>
+        </div>
+        </div>
+
+            
+            
+            
+            
+            
+            
+
+            <div class="col-sm-3"  style = "margin:0%; padding:0%;">
+            <div class="card"> 
+                
                     <img class="background-image"  style = "background-image:url(public/Assets/images/ArmoireV.jpg); min-height:1000px;background-attachment:fixed;
   background-position:center; background-repeat:no-repeat; background-size:cover; z-index:5;">
                 </div>
