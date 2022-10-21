953c3817299460564c6b6414a1f4967a85cf735d
Samuel Da Silva Leitao <dasilvas@3il.fr>
2022-10-21 14:26:47 +0200

    Images qui depassent samer

--- views/Visiteur/Rap.view.php
+++ views/Visiteur/Rap.view.php
@@ -12,38 +12,38 @@
         <div class="col-sm-6 ">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-12 g-12">
             <div class="col-sm-6" >
-                 <div class="card" style="margin: 5px;">
-                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+                 <div class="card" style="margin: 100px; ">
+                      <img class="card-img-top" src="public\Assets\images\Album\Rock\ACDC\TRE.jpg" alt="Card image cap">
                            <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Some quick example .</p>
                                        <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
             </div>
-            <div class="card" style="margin: 5px;">
-                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <div class="card" style="margin: 100px;  ">
+                      <img class="card-img-top" src="public\Assets\images\Album\Rock\Nirvanna\Nevermind.jpg" alt="Card image cap">
                            <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Some quick example .</p>
                                        <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
             </div>
             </div>
 
             <div class="col-sm-6">
-            <div class="card" style="wmargin: 5px;">
-                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <div class="card" style="margin: 100px;  ">
+                      <img class="card-img-top" src="public\Assets\images\Album\Rock\TFK\TheEndIsWhereWeBegin.jpg" alt="Card image cap">
                            <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Some quick example.</p>
                                        <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
             </div>
-            <div class="card" style="margin: 5px;">
-                      <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <div class="card" style="margin: 100px;  ">
+                      <img class="card-img-top" src="public\Assets\images\Album\Rock\TSTM\ABeautifulLie.jpg" alt="Card image cap">
                            <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Some quick example .</p>
                                        <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
             </div>
@@ -70,4 +70,3 @@
       </div>
     </div>
 </div>
-
