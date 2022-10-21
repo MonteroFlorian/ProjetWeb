2dcc2760fc865affc9f742e9bcff07c41e543c77
FlorianMontero <monterof@3il.fr>
2022-10-21 14:31:46 +0200

    otarie

--- views/Visiteur/accueil.view.php
+++ views/Visiteur/accueil.view.php
@@ -4,7 +4,7 @@
 
             <div class="col-sm-3"  style = "margin:0%; padding:0%;" >
                 <div class="card">
-                    <img class="background-image"  style = "background-image:url(public/Assets/images/Vignes.jpg); min-height:1000px;background-attachment:fixed;
+                    <img class="background-image"  style = "background-image:url(public/Assets/images/Vignes.jpg); min-height:1500px;background-attachment:fixed;
   background-position:left; background-repeat:no-repeat; background-size:cover; z-index:5;">
                 </div>
             </div>
@@ -12,13 +12,13 @@
             <div class="col-sm-6 "  style = "padding:10%; text-align:center; background-color:#FDF5E6 ;">
                         <br/>
                         <h1 class="card-title" style="color:black;"> <strong> Bienvenue sur notre site de vente de vinyles en tout genre !!! </strong></h1><br/><br/><br/>
-                        <h1 style="color:black;"> <strong> Pour vous amoureux de musique, vous pouvez maintenant commander les vinyles de vos artistes préférés </strong>  </h1>
-
+                        <h1 style="color:black;"> <strong> Pour vous amoureux de musique, vous pouvez maintenant commander les vinyles de vos artistes préférés </strong>  </h1><br/><br/><br/>
+                        <h1 style="color:black;"> <strong>"La musique donne une âme à nos coeurs et des ailes à la pensée." <br/>Platon </strong>  </h1>
             </div>
 
             <div class="col-sm-3 "  style = "margin:0%; padding:0%;">
             <div class="card">
-                    <img class="background-image"  style = "background-image:url(public/Assets/images/ArmoireV.jpg); min-height:1000px;background-attachment:fixed;
+                    <img class="background-image"  style = "background-image:url(public/Assets/images/ArmoireV.jpg); min-height:1500px;background-attachment:fixed;
   background-position:right; background-repeat:no-repeat; background-size:cover; z-index:5; ">
                 </div>
             </div>
