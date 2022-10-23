b78110295af81fc5949a3b8dbc8ffca06dd5ae9e
Nawiak <wisniewa@3il.com>
2022-10-23 18:42:08 +0200

    j'en ai marre

---
+++ views/Visiteur/Classique/Clavier_bien_tempere.view.php
@@ -0,0 +1,66 @@
+<link rel="stylesheet" href="/public
+<script src="/public/JavaScript/music.js" defer></script>
+<div class="container-fluid">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-12 g-12">
+
+        <div class="col-sm-3 "style = "margin:0%; padding:0%;">
+            <div class="card">
+                <img class="background-image"  style = "background-image:url(public/Assets/images/Vignes.jpg); min-height:1500px;background-attachment:fixed;
+                background-position:center; background-repeat:no-repeat; background-size:cover; z-index:5;">
+            </div>
+        </div>
+
+    
+        <div class="col-sm-6" style = " text-align:center; background-color:#FDF5E6 ;">
+            <br/> <br/><br/> <br/><br/> <br/>
+            <h1 class="card-title" style="color:black; text-align:center;"> <strong>Clavier bien tempéré </strong></h1>
+            <div class="player">
+                <img class="card-img-top" src="public\Assets\images\Album\Classique\JSB\JSB.jpg" alt="Card image cap">
+                <div class="wrapper">
+                    <div class="details">
+                        <div class="track-art"></div>
+                        <div class="track-name">Track Name</div>
+                        <div class="track-artist">Jean Sébastien Bach</div>
+                    </div>
+
+                    <div class="slider_container">
+                        <div class="current-time">00:00</div>
+                            <input type="range" min="1" max="100" value="0" class="seek_slider" onchange="seekTo()">
+                            <div class="total-duration">00:00</div>
+                    </div>
+
+                    <div class="slider_container">
+                        <i class="fa fa-volume-down"></i>
+                            <input type="range" min="1" max="100" value="99" class="volume_slider" onchange="setVolume()">
+                            <i class="fa fa-volume-up"></i>
+                    </div>
+
+                    <div class="buttons">
+                        <div class="playpause-track" onclick="playpauseTrack()">
+                            <i class="fa fa-play-circle fa-5x"></i>
+                        </div>
+                    </div>
+
+                        <div id="wave">
+                            <span class="stroke"></span>
+                            <span class="stroke"></span>
+                            <span class="stroke"></span>
+                            <span class="stroke"></span>
+                            <span class="stroke"></span>
+                            <span class="stroke"></span>
+                            <span class="stroke"></span>
+                        </div>  
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-sm-3"  style = "margin:0%; padding:0%;">
+            <div class="card">  
+                <img class="background-image"  style = "background-image:url(public/Assets/images/ArmoireV.jpg); min-height:1500px;background-attachment:fixed;
+                background-position:center; background-repeat:no-repeat; background-size:cover; z-index:5;">
+            </div>
+        </div>
+
+    </div>
+</div>
